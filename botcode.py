96356f1d09de9a5c7952ea668921641ffce7466f
Gamer9111 <43588367+Gamer9111@users.noreply.github.com>
2020-09-30 21:54:26 -0500

    Add files via upload

---
+++ botcode.py
@@ -0,0 +1,21 @@
+import discord, random
+from discord.ext import commands
+client=commands.Bot(command_prefix='!')
+@client.command()
+async def ?fart(ctx):
+    await ctx.send('fart')
+@client.command()
+async def ?dm(ctx):
+    await ctx.author.send('yup')
+@client.command()
+async def test(ctx):
+    replies=['bruh test ok uh commas necessary?','bruh','test','ok','uh','nah','epic','poggers',]
+    await ctx.send(random.choice(replies))
+@client.command()
+async def ?embed(ctx):
+    embed=discord.Embed()
+    embed.title='test'
+    replies=['bruh','test','test2',]
+    embed.description=random.choice(replies)
+    await ctx.send(embed=embed)
+client.run('JTjYdQxbaou1thtK6Pk9E0mg4lXD3kNB')
